6cb5d7300f8c66f24dc5b498d99d5c4c593fa2e6
Eric Shaw <ericshawmail@gmail.com>
2023-09-29 15:47:50 -0400

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>home page</h1>}></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
