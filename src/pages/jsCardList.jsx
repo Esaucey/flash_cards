295a5ea40ac40c5a7f5bf1c604d1030be1d152b9
Eric Shaw <76802221+Esaucey@users.noreply.github.com>
2023-10-22 14:01:59 -0400

    update

--- src/pages/jsCardList.jsx
+++ src/pages/jsCardList.jsx
@@ -2,10 +2,8 @@
 import React, { useState } from 'react';
 import jsData from '../data/jsCards.json';
 import Box from '@mui/system/Box';
-import Grid from '@mui/system/Unstable_Grid';
 import { Button, Container, Pagination } from '@mui/material';
 import usePagination from "../components/Pagination";
-import Card from '../components/Card'
 import ShowCards from '../components/showCards';
 
 export const JsPage = () => {
